fe47ac676811e6abef15e6528d790ed723f2e364
laylascheli <83417637+laylascheli@users.noreply.github.com>
2023-03-02 07:27:42 +0100

    Update UnionLogProblem.scala

--- src/main/scala/com/sparkTutorial/rdd/nasaApacheWebLogs/UnionLogProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/nasaApacheWebLogs/UnionLogProblem.scala
@@ -7,7 +7,7 @@ object UnionLogProblem {
     /* "in/nasa_19950701.tsv" file contains 10000 log lines from one of NASA's apache server for July 1st, 1995.
        "in/nasa_19950801.tsv" file contains 10000 log lines for August 1st, 1995
        Create a Spark program to generate a new RDD which contains the log lines from both July 1st and August 1st,
-       take a 0.1 sample of those log lines and save it to "out/sample_nasa_logs.tsv" file.
+       take a 0.1 sample of those log lines.
 
        Keep in mind, that the original log files contains the following header lines.
        host	logname	time	method	url	response	bytes
