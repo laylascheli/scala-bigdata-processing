e63e60a48959b13fab26738050809c19bc8aa8b6
laylascheli <83417637+laylascheli@users.noreply.github.com>
2023-03-02 07:27:18 +0100

    Update SameHostsProblem.scala

--- src/main/scala/com/sparkTutorial/rdd/nasaApacheWebLogs/SameHostsProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/nasaApacheWebLogs/SameHostsProblem.scala
@@ -7,7 +7,6 @@ object SameHostsProblem {
     /* "in/nasa_19950701.tsv" file contains 10000 log lines from one of NASA's apache server for July 1st, 1995.
        "in/nasa_19950801.tsv" file contains 10000 log lines for August 1st, 1995
        Create a Spark program to generate a new RDD which contains the hosts which are accessed on BOTH days.
-       Save the resulting RDD to "out/nasa_logs_same_hosts.csv" file.
 
        Example output:
        vagrant.vf.mmc.com
